017e2650db97e8db19295c328cc2adc4863d075a
Jairaj Sethi <j@jairaj.org>
2012-09-24 12:28:12 -0400

    new build process

--- build.sh
+++ build.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-VERSION="1.0"
+VERSION="1.0.0"
 LICENSE="LICENSE.txt"
 
-compressJS () {
-	REGULAR="${1}.js"
-	MINIFIED="${1}.min.js"
-	cp ${LICENSE} ${REGULAR}
-	cp ${LICENSE} ${MINIFIED}
-	cat - >> ${REGULAR}
-	cat ${REGULAR} | closure-compiler >> ${MINIFIED}
+JS_COMPILER="closure-compiler"
+CSS_COMPILER="yuicompressor --type css"
+
+mkdir -p ${VERSION}
+
+compress () {
+	cat ${LICENSE} ${3} | ${1} > ${VERSION}/${2}
 }
 
-compressCSS () {
-	REGULAR="${1}.css"
-	MINIFIED="${1}.min.css"
-	cp ${LICENSE} ${REGULAR}
-	cp ${LICENSE} ${MINIFIED}
-	cat - >> ${REGULAR}
-	cat ${REGULAR} | yuicompressor --type css >> ${MINIFIED}
-}
-
-cat clickable.js | compressJS ${VERSION}
+compress "${JS_COMPILER} " "clickable.js" "clickable.js"
